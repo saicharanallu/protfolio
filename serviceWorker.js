70d453992d3c2e15d6d57aa734be1ce70348bffd
saicharanallu <145497849+saicharanallu@users.noreply.github.com>
2023-11-01 15:35:25 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -27,3 +27,10 @@ self.addEventListener("install", installEvent => {
     })
   )
 })
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
